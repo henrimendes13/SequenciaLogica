1f1b43b33b52913c6fd74396ec2f86ca4059fbb5
Henrique Mendes <henriquemendes13@gmail.com>
2023-09-25 13:12:40 -0300

    Add project files.

---
+++ SequenciaLogica/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= x; i++)
+{
+    int y = i * i;
+    int y2 = y * i;
+    int soma = y + 1;
+    int soma2 = y2 + 1;
+
+    Console.WriteLine($"{i} {y} {y2}");
+    Console.WriteLine($"{i} {soma} {soma2}");
+}
